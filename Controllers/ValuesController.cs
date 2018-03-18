7a8296627841385a8b56f183431952641f61d877
Bo Christian Skjøtt <bo@skjoett.com>
2018-03-18 22:18:59 +0100

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebSocketWebApplication.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        int[] values = { 0, 10, 20, 30, 40, 50 };
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            foreach (int i in values)
+            {
+                yield return i.ToString();
+            }
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return $"Value at index {id} = {values[id]}";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+            values[id] = int.Parse(value);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            values[id] = -1;
+        }
+    }
+}
